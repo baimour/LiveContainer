d982a82fe51624b4df4471bb3751205cfcf0e970
baimour <85350961+baimour@users.noreply.github.com>
2025-02-24 02:45:09 +0800

    Update LCVersionInfo.m

--- LiveContainerSwiftUI/LCVersionInfo.m
+++ LiveContainerSwiftUI/LCVersionInfo.m
@@ -2,7 +2,7 @@
 
 @implementation LCVersionInfo
 + (NSString*)getVersionStr {
-    return [NSString stringWithFormat:@"Version %@-%s (%s/%s)",
+    return [NSString stringWithFormat:@"版本: %@-%s (%s/%s)",
         NSBundle.mainBundle.infoDictionary[@"CFBundleShortVersionString"],
         CONFIG_TYPE, CONFIG_BRANCH, CONFIG_COMMIT];
 }
